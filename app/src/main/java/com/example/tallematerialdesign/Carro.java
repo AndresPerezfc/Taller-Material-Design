6c056d6ef636e090838d2f16718be22dc74e94d8
andresperezfc <felipe3apc@gmail.com>
2020-06-10 23:30:09 -0500

    Interfaz agregar Carro terminada y clase datos y carro creadas

--- app/src/main/java/com/example/tallematerialdesign/Carro.java
+++ app/src/main/java/com/example/tallematerialdesign/Carro.java
@@ -1,4 +1,63 @@
 package com.example.tallematerialdesign;
 
+import java.util.ArrayList;
+
 public class Carro {
+    private String marca;
+    private String modelo;
+    private String ano;
+    private String cilindrada;
+    private int foto;
+
+    Carro(String marca, String modelo, String ano, String cilindrada, int foto){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cilindrada = cilindrada;
+        this.foto = foto;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(String cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public void guardar(){
+        Datos.guardar(this);
+    }
 }
--- app/src/main/java/com/example/tallematerialdesign/Datos.java
+++ app/src/main/java/com/example/tallematerialdesign/Datos.java
@@ -1,4 +1,14 @@
 package com.example.tallematerialdesign;
 
+import java.util.ArrayList;
+
 public class Datos {
+    private static ArrayList<Carro> carros = new ArrayList<>();
+    public static void guardar(Carro c){
+        carros.add(c);
+    }
+
+    public static ArrayList<Carro> obtener (){
+        return carros;
+    }
 }
